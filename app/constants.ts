3cd912900941bb2f8521ba9592bc7b7e710f0c2b
Jaydeep Godhani <jaydeep@Hnvk.local>
2026-04-25 23:51:36 +0530

    port changed to 3010 and 3011

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-export const serviceURL = "http://localhost:3000";
+export const serviceURL = "http://localhost:3010";
 
 export const timers: number[] = [
   12, 24, 72, 120, 168, 360, 540, 720, 900, 1080, 1260,
