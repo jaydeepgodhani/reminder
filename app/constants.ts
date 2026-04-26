5f2597f98e7e36f8924cf232c2590cd9a619b9aa
Jaydeep Godhani <jaydeep@Hnvk.local>
2026-04-26 23:06:23 +0530

    build updated

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-export const serviceURL = "http://localhost:3010";
+// export const serviceURL = "http://localhost:3010";
 
 export const timers: number[] = [
   12, 24, 72, 120, 168, 360, 540, 720, 900, 1080, 1260,
@@ -9,3 +9,8 @@ export const getNow = () => {
   now.setMilliseconds(0);
   return now;
 };
+
+export const serviceURL: string =
+  typeof window !== "undefined" && window.location.hostname === "localhost"
+    ? "http://localhost:3010"
+    : "/api";
